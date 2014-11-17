c18cea1eaa7f9da4beb91954d220416c97328f74
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2014-11-17 02:17:13 -0300

    Modify code to be compatible with Ruby 1.8.7.

--- lib/mabbre/interpreter.rb
+++ lib/mabbre/interpreter.rb
@@ -6,7 +6,7 @@ module MAbbre
 
     def method_missing(name, *args)
       matched = nil
-      if self.class.tracked_methods(MAbbre).one? {|m| matched = m if m =~ /\A#{name}/ }
+      if self.class.tracked_methods(MAbbre).one? {|m| matched = m if m.to_s =~ /\A#{name}/ }
         send(matched, *args)
       else
         super
