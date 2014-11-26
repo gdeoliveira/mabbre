00b74f0682ae45998e884d39e535f0f99a2ac8bb
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2014-11-25 22:33:08 -0300

    Alternate implementation of Compatibility.

--- lib/mabbre/compatibility.rb
+++ lib/mabbre/compatibility.rb
@@ -6,6 +6,8 @@ module MAbbre # rubocop:disable Style/Documentation
   # versions/implementations. After the environment is patched it will be automatically removed.
   module Compatibility
     class << self
+      include Constants
+
       private
 
       ##
--- lib/mabbre/compatibility/constants.rb
+++ lib/mabbre/compatibility/constants.rb
@@ -1,14 +1,18 @@
 module MAbbre
-  module Compatibility # rubocop:disable Style/Documentation
+  module Compatibility
     ##
-    # Relative path to the directory where patch files for the Object class live.
-    PATH_TO_OBJECT_PATCHES = "mabbre/patch/object_mixin".freeze
+    # This module holds constants used by MAbbre::Compatibility
+    module Constants
+      ##
+      # Relative path to the directory where patch files for the Object class live.
+      PATH_TO_OBJECT_PATCHES = "mabbre/patch/object_mixin".freeze
 
-    ##
-    # A hash of patch names for the Object class. Each patch name has a +true+ or +false+ value assigned that represents
-    # if they need to be applied or not.
-    OBJECT_PATCHES = {
-      :respond_to_missing => !Object.private_instance_methods.map(&:to_sym).include?(:respond_to_missing?)
-    }.freeze
+      ##
+      # A hash of patch names for the Object class. Each patch name has a +true+ or +false+ value assigned that
+      # represents if they need to be applied or not.
+      OBJECT_PATCHES = {
+        :respond_to_missing => !Object.private_instance_methods.map(&:to_sym).include?(:respond_to_missing?)
+      }.freeze
+    end
   end
 end
